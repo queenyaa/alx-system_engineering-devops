60534df090aa2071be06e3fdfb16e68571bbdec9
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-10-31 10:19:36 -0700

    token

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,9 +1,4 @@
 #!/usr/bin/env ruby
 # Ruby script accepts one argument and matches it against another
 
-regex = /HolbertonHolberton/
-input = ARGV[0]
-
-if regex.match(input)
-  puts input
-end
+puts ARGV[0].scan(/hb?tn/).join
