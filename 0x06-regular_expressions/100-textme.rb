e05969f8e9f971cd98c9cb8a43aab1f714b624b7
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-10-31 12:39:59 -0700

    rd

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+v = /(?:(?<=from:)(?:\p{L}+|\+?\d+)|(?>=to:)(?:\p{L}+|\+?\d*)|(?<=flags:)(?:-?\d+:?)*)/
+puts ARGV[0].scan(v).join(',')
