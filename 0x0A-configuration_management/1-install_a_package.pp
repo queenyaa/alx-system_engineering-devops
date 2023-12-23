56c798e048cdc85bfdb2587a572bfc73031922f2
queenyaa <ysgyan@gmail.com>
2023-12-22 18:05:56 -0800

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 # Puppet manifest file to install a package
 
+$packages = [ 'python3', 'python3-pip' ]
+
+package { $packages:
+  ensure => 'installed',
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip']
 }
