495df9b6e1891b81294663671e49ef85ac9f16b9
queenyaa <ysgyan@gmail.com>
2023-12-22 16:23:36 -0800

    0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # a Puppet manifest file
 
 file { '/tmp/school':
-   ensure	=> 'file',
-   mode		=> '0744',
-   owner	=> 'www-data',
-   group	=> 'www-data',
-   content	=> 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
