ca336e501ae2970956dd7c7887a8be98a136646f
queenyaa <ysgyan@gmail.com>
2023-12-25 15:18:42 -0800

    4'

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,23 @@
 # ssh configuration with Puppet
 
-augeas { 'ssh_config_password_auth':
-  context => '/files/etc/ssh/ssh_config',
-  changes => ['set Host[1]/PasswordAuthentication no'],
+file { '/home/ubuntu/.ssh/':
+  ensure => directory,
 }
 
-augeas { 'ssh_config_identity_file':
-  context => '/files/etc/ssh/ssh_config',
-  changes => ['set Host[1]/IdentityFile ~/.ssh/school'],
+file { '/home/ubuntu/.ssh/config':
+  ensure  => present,
+  content => "Host *\n
+               IdentityFile ~/.ssh/school\n
+               PasswordAuthentication no\n",
+  replace => true,
 }
+
+# augeas { 'ssh_config_password_auth':
+  # context => '/files/etc/ssh/ssh_config',
+  # changes => ['set Host[1]/PasswordAuthentication no'],
+# }
+
+# augeas { 'ssh_config_identity_file':
+  # context => '/files/etc/ssh/ssh_config',
+  # changes => ['set Host[1]/IdentityFile ~/.ssh/school'],
+# }
