8071faaffa655fd51141e17c5ccccf85752c8361
Yaa Serwaa Gyan <ysgyan@gmail.com>
2023-12-25 11:23:13 -0800

    4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# using Puppet to make changes to configuration file
+
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path    => 'etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  path    => 'etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
