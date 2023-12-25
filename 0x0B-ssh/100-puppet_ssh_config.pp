36acdce412b7b17fd10ea6eacbf6a0798c4e5dde
queenyaa <ysgyan@gmail.com>
2023-12-25 12:39:45 -0800

    4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 # using Puppet to make changes to configuration file
 
-include stdlib
-
-file_line { 'Turn off passwd auth':
-  path    => 'etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true,
-}
-
-file_line { 'Declare identity file':
-  path    => 'etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
-  replace => true,
-}
+augeas { 'ssh_config':
+  context => '/etc/ssh/ssh_config',
+  changes => [
+    'set Host/* SendEnv LANG LC_*',
+    'set Host/* HashKnownHosts yes',
+    'set Host/* GSSAPIAuthentication yes',
+    'set Host/* GSSAPIDelegateCredentials no',
+    'set Host/* IdentityFile ~/.ssh/school',
+    'set Host/* PasswordAuthentication no',
+  ],
+ }
