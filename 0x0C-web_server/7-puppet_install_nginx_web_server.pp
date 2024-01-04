9a22e0e6012dbce37b553378aa47ac505d5cf519
Yaa Serwaa Gyan <ysgyan@gmail.com>
2024-01-03 20:01:54 -0800

    4

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,32 +1,21 @@
 # Using puppet install nginx
 
 package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'install':
   ensure => 'present',
-}
-
-exec { 'update-package-lists':
-  command     => 'sudo apt-get update',
-  refreshonly => true,
-}
-
-file ( '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "# Nginx Server Configuration\n\nserver {\n\tlisten 80 default_server;\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/;\n\t}\n}\n",
-  notify  => Service['nginx'],
-}
-
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.nginx-debian.html':
-  ensure  => file,
   content => 'Hello World!',
-  notify  => Service['nginx'],
 }
 
 service { 'nginx':
-  ensure => running,
-  enable => true,
+  ensure  => running,
+  require => Package['nginx'],
 }
