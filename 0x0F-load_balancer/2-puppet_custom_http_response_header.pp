48b8ddf88ec477aa7114114f1534aab1750d2869
queenyaa <ysgyan@gmail.com>
2024-01-08 21:41:50 -0800

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,11 +9,11 @@ file { '/var/www/html/index.html':
 }
 
 exec { 'command':
-  command => 'sed -i "51i \\\tadd_header X-Served-By: $HOSTNAME;" /etc/nginx/sites-enabled/default && service nginx restart',
+  command  => 'sed -i "51i \\\tadd_header X-Served-By: $HOSTNAME;" /etc/nginx/sites-enabled/default && service nginx restart',
   provider => shell,
-  path => '/usr/bin:/bin', # Set the PATH to include binaries
-  unless => 'grep -q "add_header X-Served-By: $HOSTNAME;" /etc/nginx/sites-enabled/default',
-  require => Package['nginx'],
+  path     => '/usr/bin:/bin', # Set the PATH to include binaries
+  unless   => 'grep -q "add_header X-Served-By: $HOSTNAME;" /etc/nginx/sites-enabled/default',
+  require  => Package['nginx'],
 }
 
 service { 'nginx':
