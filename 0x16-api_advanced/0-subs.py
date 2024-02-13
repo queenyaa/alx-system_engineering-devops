fee698d1f70d278738bee1bba2bfb906d8601b4c
queenyaa <ysgyan@gmail.com>
2024-02-13 11:29:18 -0800

    followers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers
+for a given subreddit
+"""
+
+import requests
+from sys import argv
+import json
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit
+    Args:
+        subreddit (str): the name of the subreddit
+    Returns:
+        int: the number of subscribers, or 0 if the subreddit is invalid
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {
+            'User-Agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; '
+            'rv:122.0) Gecko/20100101 Firefox/122.0')}
+    response = requests.get(url, headers=header, allow_redirects=False)
+    # print("Response status code:", response.status_code)
+    # data = response.json()
+    if response.status_code >= 300:
+        return (0)
+    json = response.json()
+    data_dict = json.get('data')
+    return(data_dict.get('subscribers'))
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,4 @@
+# Readme of 0x16-api_advanced
+---
+
+
