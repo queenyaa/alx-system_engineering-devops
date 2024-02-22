05a03d918d9b2c95a569890c997dad4ec05aa931
queenyaa <ysgyan@gmail.com>
2024-02-22 07:33:51 +0000

    open as many files

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,11 +2,6 @@
 
 $user = 'holberton'
 
-# Ensure the limits.conf file exists
-file { '/etc/security/limits.conf':
-  ensure => file,
-}
-
 # Set the file descriptor limits using exec
 exec { 'change-os-configuration-for-holberton-user':
   command => "sed -i '/$user hard/s/5/10000/; /$user soft/s/4/15000/' /etc/security/limits.conf",
