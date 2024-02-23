c77d4f4829e0f7d9bfc1532d81f34d9fab96b36e
queenyaa <ysgyan@gmail.com>
2024-02-23 13:36:47 +0000

    satifying puppet-lint

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -4,6 +4,6 @@ $user = 'holberton'
 
 # Set the file descriptor limits using exec
 exec { 'change-os-configuration-for-holberton-user':
-  command => "sed -i '/$user hard/s/5/10000/; /$user soft/s/4/15000/' /etc/security/limits.conf",
+  command => "sed -i '/${user} hard/s/5/10000/; /${user} soft/s/4/15000/' /etc/security/limits.conf",
   path    => '/usr/locla/bin/:/bin/'
 }
